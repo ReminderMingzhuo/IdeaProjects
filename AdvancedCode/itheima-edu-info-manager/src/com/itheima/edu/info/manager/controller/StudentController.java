2b9c674681511a285cef8ba1333a40abdd678dee
mingzhuo <mingzhuo@usc.edu>
2022-06-28 17:02:29 -0700

    '网络编程'

---
+++ AdvancedCode/itheima-edu-info-manager/src/com/itheima/edu/info/manager/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.itheima.edu.info.manager.controller;
+
+public class StudentController {
+    //开启学生管理系统，并展示学生管理系统
+    public void start() {
+        System.out.println("--------欢迎来到 <学生> 管理系统--------");
+        System.out.println("请输入您的选择: 1.添加学生  2.删除学生  3.修改学生  4.查看学生  5.退出");
+        
+    }
+}
