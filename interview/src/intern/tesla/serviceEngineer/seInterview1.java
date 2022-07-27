295da34ba9a988cacdc7f86e9587b7e068f86ed5
mingzhuo <mingzhuo@usc.edu>
2022-07-27 15:21:17 -0700

    ''

--- interview/src/intern/tesla/serviceEngineer/seInterview1.java
+++ interview/src/intern/tesla/serviceEngineer/seInterview1.java
@@ -1,4 +1,15 @@
 package intern.tesla.serviceEngineer;
 
 public class seInterview1 {
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        int result = s1.addTwo(1,2);
+        System.out.println("result"+result);
+    }
+}
+
+class Solution{
+    public int addTwo(int i,int j){
+        return i+j;
+    }
 }
